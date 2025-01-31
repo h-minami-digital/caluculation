1baea89ac2d0951aeba63cda23442d0e8d6dfd4a
h-minami-digital <h.minami@digital-standard.com>
2025-01-31 18:26:57 +0900

    条件式を変更
    条件式を変更

--- bene_whitening.js
+++ bene_whitening.js
@@ -3,7 +3,14 @@
 
 
 
-
+function fff(human_cost,room_cost,business,percent,time,installtion){
+    if(!human_cost||!room_cost||!business||!percent||!time||!installtion){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
 
 
@@ -12,6 +19,11 @@
 function calcStart(){
     console.log("debug");
     //変数を宣言(文字型を数字型に変換)
+    
+    if(fff(human_cost,room_cost,business,percent,time,installtion)== false){
+        alert("必須項目を入力してください");
+        return;
+    }
     var installtion_str = document.getElementById("installtion_number").value;
     var installtion = parseInt(installtion_str.replace(/,/g,""));
 
@@ -45,6 +57,10 @@ function calcStart(){
     var month2_str = document.getElementById("sufficiency_month2").value;
     var month2 = parseInt(month2_str.replace(/,/g,""));
 
+    
+
+    
+
     //最大施術可能数
     monthly_maxtreatment = max(installtion,business,time);
     
@@ -76,7 +92,10 @@ function calcStart(){
 
     month2_str = month2.toLocaleString();
     document.getElementById("sufficiency_month2").value = month2_str;
+
+   
 }
+
    //表内の計算メソッド
     function max(installtion,business,time){
         return installtion * business * time;
@@ -105,61 +124,5 @@ function calcStart(){
     }
 
     //エラー発生時のメソッド
-    function mistake(){
-        //計算するための最低条件
-        if(!human_cost){
-           alert('必須項目を全て入力してください');
-           return ;
-        }
-   
-       if(!room_cost){
-           alert('必須項目を全て入力してください');
-           return ;
-        }
-   
-       if(!business){
-           alert('必須項目を全て入力してください');
-           return ;
-        }
-
-       if(!percent){
-        alert('必須項目を全て入力してください');
-        return ;
-        }
-
-        if(!time){
-            alert('必須項目を全て入力してください');
-            return ;
-        }
-
-       if(installtion<0){
-           alert('0より小さい数字は入力が出来ません')
-           return ;
-        }
-
-       if(business<0){
-        alert('0より小さい数字は入力が出来ません')
-        return ;
-        }
-
-        if(time<0){
-        alert('0より小さい数字は入力が出来ません')
-        return ;
-        }
-
-        if(percent<0){
-        alert('0より小さい数字は入力が出来ません')
-        return ;
-        }
-
-        if(room_cost<0){
-        alert('0より小さい数字は入力が出来ません')
-        return ;
-        }
-
-        if(installtion>10){
-        alert('')
-        }
-   
-   }
+    
    
